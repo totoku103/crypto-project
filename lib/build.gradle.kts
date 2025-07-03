2cf37737aeac0875979ab2cb3c18aefe072639ea
김수홍 <totoku103@burunet.co.kr>
2025-07-04 08:02:34 +0900

    chore: add jar task

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -40,3 +40,13 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.jar {
+    archiveBaseName.set("crypto")
+    archiveVersion.set("1.0.0")
+    manifest {
+        attributes(
+            "Main-Class" to "me.totoku103.crypto"
+        )
+    }
+}
