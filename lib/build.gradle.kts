31cd009287eefbe084918116eb09e7ec0823851b
김수홍 <totoku103@gmail.com>
2026-06-22 13:44:14 +0900

    chore: 배포 버전 1.0.3으로 업그레이드
    KISA SHA-256 무한루프/제로패딩 버그 수정 및 Gradle 9.6 업그레이드를 포함한
    1.0.3 릴리스. archiveVersion, publication version, publishToGitHub 버전을 모두 갱신.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -47,7 +47,7 @@ tasks.named<Test>("test") {
 
 tasks.jar {
     archiveBaseName.set("crypto")
-    archiveVersion.set("1.0.2")
+    archiveVersion.set("1.0.3")
 }
 
 publishing {
@@ -55,7 +55,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "me.totoku103"
             artifactId = "crypto"
-            version = "1.0.2"
+            version = "1.0.3"
 
             from(components["java"])
         }
@@ -84,7 +84,7 @@ tasks.register("publishToGitHub") {
 
     val injected = project.objects.newInstance<InjectedExecOps>()
     val repoDir = file("/Users/totoku103/Project-Private/totoku103-maven-repository/releases")
-    val version = "1.0.2"
+    val version = "1.0.3"
 
     doLast {
         injected.execOps.exec {
